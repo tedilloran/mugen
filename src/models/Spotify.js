acd982721d95e678b66f90fa5bc96389f43677d9
Trent Edilloran <trent.edilloran@gmail.com>
2019-10-11 04:38:43 +0000

    Successfully acquired Spotify access token

--- src/models/Spotify.js
+++ src/models/Spotify.js
@@ -5,18 +5,7 @@ const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const encodedCredentials = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 const headers = { 'Authorization': `Basic ${encodedCredentials}` };
 const data = qs.stringify({ 'grant_type': 'client_credentials' });
-const SpotifyAPIInit = { method: 'POST', headers, data };
-
-SpotifyInit = async () =>  {
-  console.log('Getting Spotify access token...');
-  try {
-    let res = await axios.post('https://accounts.spotify.com/api/token', SpotifyAPIInit);
-  } catch(err) {
-    console.log('Error occured while acquired access token');
-    console.log(err);
-  }
-  console.log('Acquired Spotify token');
-}
+const SpotifyAPIInit = { method: 'POST', headers };
 
 const Spotify = {
   getTrackAudioAnalysis: (id) => {
@@ -28,7 +17,20 @@ const Spotify = {
   getTracks: (idList) => {
   },
   getTrack: (id) => {
-  },
+  }
 };
+
+async function SpotifyInit() {
+  console.log('Getting Spotify access token...');
+  let res = await axios.post('https://accounts.spotify.com/api/token', data,  SpotifyAPIInit);
+  console.log('Acquired Spotify token');
+  console.log(res);
+}
+
+SpotifyInit()
+  .catch((err) => {
+    console.log('Error occured while acquiring access token');
+    console.log(err);
+  });
 
 export default Spotify;
